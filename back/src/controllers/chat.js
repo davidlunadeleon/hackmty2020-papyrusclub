bd9327dc693559c25340dff7a547e12d97391c88
davidlunadeleon <davidlunadeleon@gmail.com>
2020-08-29 20:00:16 -0500

    [Back] Add a route to posts messages in a chat

--- back/src/controllers/chat.js
+++ back/src/controllers/chat.js
@@ -8,6 +8,31 @@ chatRouter.route('/user/:id').get(async (req, res) => {
 	res.json(chats);
 });
 
+chatRouter.route('/:id').post(async (req, res) => {
+	const body = req.body;
+	const chat = await Chat.findById(req.params.id);
+	if (!body.userId || !body.message) {
+		return res.status(400).json({ error: 'Information missing.' });
+	} else if (!chat) {
+		return res.status(404).json({ error: 'Chat not found.' });
+	} else if (
+		!chat.usuarios.find((u) => u.toString() === body.userId.toString())
+	) {
+		return res.sendStatus(401);
+	}
+
+	await Chat.findByIdAndUpdate(req.params.id, {
+		$push: {
+			mensajes: {
+				usuario: body.userId,
+				contenido: body.message,
+				fecha: new Date()
+			}
+		}
+	});
+	res.sendStatus(201);
+});
+
 chatRouter.route('/').post(async (req, res) => {
 	const body = req.body;
 	const userIds = await Promise.all(
